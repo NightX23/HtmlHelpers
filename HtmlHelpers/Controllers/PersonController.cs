1a790426db9054058cfa4ea54959fa88113bce4f
Marlon <marlon231098@hotmail.com>
2019-03-29 22:09:36 -0400

    root

---
+++ HtmlHelpers/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HtmlHelpers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HtmlHelpers.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(Person p)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(p);
+            }
+            return View();
+
+        }
+    }
+}
---
+++ HtmlHelpers/Models/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HtmlHelpers.Models
+{
+    public class Person
+    {
+        [Required]
+        [Display(Name ="Cédula")]
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Apellido")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Range(15,99)]
+        [Display(Name = "Edad")]
+        public int Age { get; set; }
+
+        [Display(Name = "Teléfono")]
+        public string Phone { get; set; }
+
+        [EmailAddress]
+        [Display(Name = "Correo")]
+        public string Email { get; set; }
+
+        [Display(Name = "Género")]
+        public Gender Gender { get; set; }
+
+        [Display(Name = "Estado Civil")]
+        public string CivilStatus { get; set; }
+
+       // [Display(Name = "Afición")]
+      //  public string Hobby  { get; set; }
+    }
+
+    public enum Gender
+    {
+        M,
+        F
+    }
+}
